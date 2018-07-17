99a7d93efab1c0a941e06a305e4936cf1448d89c
Your Name <you@example.com>
2018-07-17 16:31:01 +0700

    initial commit

---
+++ Session07/get_even_list.py
@@ -0,0 +1,11 @@
+def get_even_list(l=[]):
+    for i in l:
+        if l[i]%2 is not 0:
+            l.remove(i)
+                 
+even_list = get_even_list([1, 2, 5, -10, 9, 6])
+
+if set(even_list) == set([2, -10, 6]):
+    print("Your function is correct")
+else:
+    print("Ooops, bugs detected")
